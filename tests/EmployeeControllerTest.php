8ebf08c059fbc8f9a7f11f4d5c70b7dfd9f88574
Dominik <d.jasinski@ideo.pl>
2024-07-22 18:35:34 +0200

    feat: create EmployeeControllerTest

---
+++ tests/EmployeeControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class EmployeeControllerTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createClient();
+
+        $jsonData = json_encode([
+            'firstName' => 'Dominik',
+            'lastName' => 'Test'
+        ]);
+
+        $headers = [
+            'HTTP_CONTENT_TYPE' => 'application/json',
+            'HTTP_ACCEPT' => 'application/json',
+        ];
+
+        $client->request('POST', '/v1/employees', [], [], $headers, $jsonData);
+
+        $this->assertResponseIsSuccessful();
+    }
+}
